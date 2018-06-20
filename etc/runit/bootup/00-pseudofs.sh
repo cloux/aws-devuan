9aae41a0c168437db9e3d287c9700a503c056b1d
Jan <cloux@rote.ch>
2018-06-20 12:48:30 +0200

    v1.5.1
     - fix: mount rpc_pipefs at bootup
     - fix: check rpc_pipefs in rpc.gssd, rpc.idmapd
     - fix: rpc.statd runscript
     - change: cleaner idmapd start/stop behavior
     - add: blkmapd runscript

--- etc/runit/bootup/00-pseudofs.sh
+++ etc/runit/bootup/00-pseudofs.sh
@@ -6,9 +6,12 @@ mountpoint -q /proc || mount -o nosuid,noexec,nodev -t proc proc /proc
 mountpoint -q /sys || mount -o nosuid,noexec,nodev -t sysfs sys /sys
 mountpoint -q /run || mount -o mode=0755,nosuid,nodev -t tmpfs run /run
 mountpoint -q /dev || mount -o mode=0755,nosuid -t devtmpfs dev /dev
-mkdir -p -m0755 /run/runit /run/lvm /run/user /run/lock /run/log /dev/pts /dev/shm
+mkdir -p -m0755 /run/runit /run/lvm /run/user /run/lock /run/log /run/rpc_pipefs /dev/pts /dev/shm
 mountpoint -q /dev/pts || mount -o mode=0620,gid=5,nosuid,noexec -n -t devpts devpts /dev/pts
 mountpoint -q /dev/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /dev/shm
+# The rpc_pipefs is required by blkmapd, rpc.idmapd, etc.
+# Old path was /var/lib/nfs/rpc_pipefs, the new default is /run/rpc_pipefs
+mountpoint -q /run/rpc_pipefs || mount -o nosuid,noexec,nodev -t rpc_pipefs rpc_pipefs /run/rpc_pipefs
 
 if [ -z "$VIRTUALIZATION" ]; then
 	mountpoint -q /sys/fs/cgroup || mount -o mode=0755 -t tmpfs cgroup /sys/fs/cgroup
