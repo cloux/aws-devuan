cce63a2c387dc4ae18a2ac6aa792eefedcd934f7
Jan <cloux@rote.ch>
2018-09-20 16:43:41 +0200

    v1.6.8
     - fix: agetty cleared the console on tty1
     - fix: incron runscript disabled bootlogd
       NOTE: incron will now pollute the console, if socklog-unix can't run
     - change: ntp runscript and public-ip query script
     - add: etc/runit/bootup/11-printinfo.sh
       to print additional system information during bootup, e.g. IPs

---
+++ etc/runit/bootup/11-printinfo.sh
@@ -0,0 +1,11 @@
+# *-*- Shell Script -*-*
+# Print additional system info to the console
+#
+# (cloux@rote.ch)
+
+#LOCAL_IP="$(hostname -I 2>/dev/null)"
+#if [ "$LOCAL_IP" ]; then
+#	msg " Local IP: $LOCAL_IP"
+#	PUBLIC_IP="$(/usr/local/bin/public-ip 2>/dev/null)"
+#	[ "$PUBLIC_IP" ] && msg "Public IP: $PUBLIC_IP"
+#fi
