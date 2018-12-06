60df3bf676a6253694cbb6a0c056afdaeb739c3d
Jan <cloux@rote.ch>
2018-12-06 02:26:20 +0100

    v2.2.1
     - fix: bootup/00-pseudofs.sh - link /run/shm to /dev/shm for
       compatibility reasons, some apps may crash without it

--- etc/runit/bootup/00-pseudofs.sh
+++ etc/runit/bootup/00-pseudofs.sh
@@ -15,6 +15,9 @@ mkdir -p -m0755 /run/shm /run/lvm /run/user /run/lock /run/log /run/rpc_pipefs
 mountpoint -q /run/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /run/shm
 mountpoint -q /run/rpc_pipefs || mount -o nosuid,noexec,nodev -t rpc_pipefs rpc_pipefs /run/rpc_pipefs
 
+# link /dev/shm to /run/shm for compatibility
+ln -sf /run/shm /dev/
+
 if [ -z "$VIRTUALIZATION" ]; then
 	mountpoint -q /sys/fs/cgroup || mount -o mode=0755 -t tmpfs cgroup /sys/fs/cgroup
 	awk '$4 == 1 { system("mountpoint -q /sys/fs/cgroup/" $1 " || { mkdir -p /sys/fs/cgroup/" $1 " && mount -t cgroup -o " $1 " cgroup /sys/fs/cgroup/" $1 " ;}" ) }' /proc/cgroups
