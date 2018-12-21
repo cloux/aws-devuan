4079537397b07f35cb53d724fa2a120106d92eb8
Jan <cloux@rote.ch>
2018-12-21 13:49:03 +0100

    v2.2.6
     - change: nfs-check.sh - add second verification for stale mount

--- tools/nfs-check.sh
+++ tools/nfs-check.sh
@@ -5,20 +5,25 @@
 # (cloux@rote.ch)
 
 # how long to wait for NFS to respond before considered stale
-NFS_TIMEOUT=20
+NFS_TIMEOUT=10
 
-printf "nfs-check started: %s\n" "$(date --iso-8601=ns)"
-
+#printf "nfs-check started: %s\n" "$(date --iso-8601=ns)"
 RET=0
 for NFS_MOUNT in $(grep ' nfs' /proc/mounts | cut -d ' ' -f 2); do
 	timeout --kill-after=1 $NFS_TIMEOUT ls -1 "$NFS_MOUNT" >/dev/null
 	if [ $? -eq 0 ]; then
 		printf "AVAILABLE: '%s'\n" "$NFS_MOUNT"
 	else
-		printf "STALE: '%s'\n" "$NFS_MOUNT"
-		RET=$(($RET+1))
+		# appears stale, check again
+		timeout --kill-after=1 2 ls -1 "$NFS_MOUNT" >/dev/null
+		if [ $? -eq 0 ]; then
+			printf "BUSY: '%s'\n" "$NFS_MOUNT"
+		else
+			printf "STALE: '%s'\n" "$NFS_MOUNT"
+			RET=$(($RET+1))
+		fi
 	fi
 done
-exit $RET
+#printf "nfs-check finished: %s\n" "$(date --iso-8601=ns)"
 
-printf "nfs-check finished: %s\n" "$(date --iso-8601=ns)"
+exit $RET
